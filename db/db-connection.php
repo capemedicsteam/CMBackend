55364769f24295388a169eb4a50e77089ffc81b4
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-07-23 21:51:43 +0200

    Fixed merge error

---
+++ db/db-connection.php
@@ -0,0 +1,11 @@
+<?php 
+ function openDBConnection()
+  {
+    $server = "remotemysql.com";
+    $username = "dDsScbv5Ai";
+    $password = "pFCtiZ0AnC";
+    $database = "dDsScbv5Ai";
+    return new mysqli($server, $username, $password, $database);
+  }
+  $conn = openDBConnection();
+?>
