fff586d9ea38cf609cfda2db4caec8d77874f9f7
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-08-21 23:06:42 +0200

    Added check for user before creation

--- scripts/RegisterCustomer.php
+++ scripts/RegisterCustomer.php
@@ -5,13 +5,19 @@
   //Check if all required data is present
   if(!isset($_GET['name']) || !isset($_GET['surname']) || !isset($_GET['email']) || !isset($_GET['company']) || !isset($_GET['number']) || !isset($_GET['password']))
   {
-    echo($twig->load("action-result.json")->render(["result" => "failure"]));
+    echo($twig->load("action-result.json")->render(["result" => "error_incomplete_data"]));
     exit();
   }
   //Create customer
   $customer = new Customer($_GET["name"], $_GET["surname"], $_GET["number"], $_GET['company'], $_GET["email"]);
   $entityManager->persist($customer);
   $entityManager->flush();
+  //Check if email address has already been used
+  if($entityManager->find("Customer", $_GET["email"])
+  {
+    echo($twig->load("action-result.json")->render(["result" => "error_duplicate_email"]));
+    exit();
+  }
   //Create user
   $user = new User($_GET['email'], $_GET['password'], "Customer", $customer->getCustomerId());
   $entityManager->persist($user);
