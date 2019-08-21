785f5dd6fa6dd14cbcd871c215f65fb77c379e90
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-08-21 23:07:18 +0200

    ****

--- inclusions/ConfigureTwig.php
+++ inclusions/ConfigureTwig.php
@@ -1,6 +1,5 @@
 <?php
   require_once '../vendor/autoload.php';
-
   $loader = new \Twig\Loader\FilesystemLoader('../json');
   $twig = new \Twig\Environment($loader, [
       'cache' => '../twig-cache',
