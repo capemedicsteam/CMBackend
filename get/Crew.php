09e88369e19f06768507693d6aa1cf8843abe7e9
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-09-21 22:05:48 +0200

    Added get for crew

---
+++ get/Crew.php
@@ -0,0 +1,37 @@
+<?php
+  //Load Dependencies
+  session_start();
+  require_once "../db/db-connection.php";
+  require_once "../inclusions/ConfigureTwig.php";
+  require_once "../inclusions/Common.php";
+  $crew = $entityManager->getRepository("Crew")->findAll();
+  $count = 0;
+  foreach($crew as $member)
+  {
+    $output[$count]["id"] = $member->getCrewId();
+    $output[$count]["name"] = $member->getCrewName();
+    $output[$count]["surname"] = $member->getCrewSurname();
+    $output[$count]["number"] = $member->getContactNumber();
+    $output[$count]["email"] = $member->getEmail();
+    $qualification = "";
+    if($member->isFire())
+    {
+      $qualification = $qualification."Fire/";
+    }
+    if($member->isSafety())
+    {
+      $qualification = $qualification."Safety/";
+    }
+    if($member->isMedical())
+    {
+      $qualification = $qualification."Medical/";
+    }
+    if(strlen($qualification) == 0)
+    {
+      $qualification = "None/";
+    }
+    $output[$count]["qualification"] = substr_replace($qualification, "", -1);
+    $count = $count + 1;
+  }
+  echo($twig->load("get-crew.json")->render(["crew" => $output]));
+?>
