b7a2030cad53781622dee17d6e878a9b4339d936
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-09-28 23:12:01 +0200

    Obsolete

--- admin/pageload-bookingsManagementPage.php
+++
@@ -1,7 +0,0 @@
-<?php
-  require_once("../db/db-connection.php");
-  $customer = new Customer("Dillon", "Pillay", "084 915 7900", "GCX", "email");
-  $entityManager->persist($customer);
-  $entityManager->flush();
-  echo("Customer created.");
-?>
