4dc33054cea936597c0e01ad5745b6b8ebdd07e7
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-12-21 13:34:13 +0200

    Added name, surname and id to return JSON

--- scripts/Login.php
+++ scripts/Login.php
@@ -20,7 +20,21 @@
     $_SESSION["email"] = $user->getEmail();
     $_SESSION["user"] = $user->getUserID();
     $_SESSION["userType"] = $user->getType();
-    echo($twig->load("user-login.json")->render(["type" => $user->getType()]));
+    $name = "admin";
+    $surname = "admin":
+    if($_SESSION["userType"] == "Crew")
+    {
+      $crew = $entityManager->find("Crew", $_SESSION["user"]);
+      $name = $crew->getCrewName();
+      $surname = $crew->getCrewSurname();
+    }
+    else if($_SESSION["userType"] == "Customer")
+    {
+      $customer = $entityManager->find("Customer", $_SESSION["user"]);
+      $name = $customer->getCustomerName();
+      $surname = $customer->getCustomerSurname();
+    }
+    echo($twig->load("user-login.json")->render(["type" => $user->getType(), "name" => $name, "surname" => $surname, "id" => $_SESSION["user"]]));
     exit();
   }
   echo($twig->load("action-result.json")->render(["result" => "error_password_incorrect"]));
