de48130cee630d5782f057e0c92a2cd52394799b
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2019-12-22 20:43:18 +0200

    Bug fix

--- scripts/Contact.php
+++ scripts/Contact.php
@@ -8,13 +8,12 @@
     echo($twig->load("action-result.json")->render(["result" => "error_incomplete_data"]));
     exit();
   }
-  $message = "";
+  $message = $twig->load("contact.html")->render(["name" => $_GET["name"], "surname" => $_GET["surname"], "email" => $_GET["email"], "message" => $_GET["message"]]);
   if(mail("pmphareng@gmail.com", "Contact Form", $message, "From: ".$_GET["email"]))
   {
     echo($twig->load("action-result.json")->render(["result" => "success"]));
     exit();
   }
-  echo($twig->load("contact.html")->render(["name" => $_GET["name"], "surname" => $_GET["surname"], "email" => $_GET["email"], "message" => $_GET["message"]]));
 
 
 ?>
