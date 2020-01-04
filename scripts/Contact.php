bdc8a107d6dea3e8ea7439237261847eedd5f5ee
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2020-01-04 16:34:03 +0200

    Changed headers field in mail

--- scripts/Contact.php
+++ scripts/Contact.php
@@ -9,7 +9,8 @@
     exit();
   }
   $message = $twig->load("contact.html")->render(["name" => $_GET["name"], "surname" => $_GET["surname"], "email" => $_GET["email"], "message" => $_GET["message"]]);
-  if(mail("pmphareng@gmail.com", "Contact Form", $message, "From: ".$_GET["email"]))
+  $headers = "From: ".$_GET["email"]."\r\nContent-type: text/html;charset=UTF-8";
+  if(mail("pmphareng@gmail.com", "Contact Form", $message, $headers))
   {
     echo($twig->load("action-result.json")->render(["result" => "success"]));
     exit();
