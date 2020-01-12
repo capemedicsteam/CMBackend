660124193d46cc666b292c715c7ce1a54b496cff
Dillon Diego Pillay <dillondiegopillay@gmail.com>
2020-01-12 02:07:34 +0200

    Added changes requested by apk

--- scripts/MedicalForm.php
+++ scripts/MedicalForm.php
@@ -6,19 +6,26 @@
   require_once "../inclusions/ConfigureTwig.php";
   require_once "../inclusions/Common.php";
   //Check if all required data is present -need type value comment in values
-  if(!isset($_GET["jobId"]) || !isset($_GET["patIdPassport"]) || !isset($_GET["patName"]) || !isset($_GET["patSurname"]) || !isset($_GET["answers"]))
+  if(!isset($_GET["jobId"]) || !isset($_GET["medicalForm"]))
+  {
+    echo($twig->load("action-result.json")->render(["result" => "error_incomplete_data"]));
+    exit();
+  }
+  //Check for required patient data
+  $data = json_decode($_GET["medicalForm"]);
+  if(!isset($data["Patient Details"]["ID"]) || !isset($data["Patient Details"]["Full_name"]) || !isset($data["Patient Details"]["Surname"]))
   {
     echo($twig->load("action-result.json")->render(["result" => "error_incomplete_data"]));
     exit();
   }
   //Save File
-  $file = fopen("../files/prf/".$_GET["jobId"]."_".$_GET["patIdPassport"]."_".$_GET["patName"]." ".$_GET["patSurname"], "w");
+  $file = fopen("../files/prf/".$_GET["jobId"]."_".$data["Patient Details"]["ID"]."_".$data["Patient Details"]["Full_name"]." ".$data["Patient Details"]["Surname"], "w");
   if($file == false)
   {
     echo($twig->load("action-result.json")->render(["result" => "error_additional_data"]));
     exit();
   }
-  fwrite($file, $_GET["answers"]);
+  fwrite($file, $_GET["medicalForm"]);
   fclose($file);
   echo($twig->load("action-result.json")->render(["result" => "success"]));
 ?>
